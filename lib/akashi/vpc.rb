c2c295b2304e0dfc91fc0740712a9694fb0aeb10
remain <ponde.ponde.ponde@gmail.com>
2014-06-10 02:37:01 +0900

    Ooops! :scream:

--- lib/akashi/vpc.rb
+++ lib/akashi/vpc.rb
@@ -1,6 +1,7 @@
 require "akashi/vpc/base"
 require "akashi/vpc/internet_gateway"
 require "akashi/vpc/route_table"
+require "akashi/vpc/security_group"
 require "akashi/vpc/subnet"
 
 module Akashi
--- lib/akashi/vpc/security_group.rb
+++ lib/akashi/vpc/security_group.rb
@@ -1,3 +1,9 @@
+require "akashi/vpc/security_group/base"
+require "akashi/vpc/security_group/elb"
+require "akashi/vpc/security_group/rds"
+require "akashi/vpc/security_group/ssh_gateway"
+require "akashi/vpc/security_group/web_server"
+
 module Akashi
   class Vpc
     class SecurityGroup < Akashi::Vpc::Base
