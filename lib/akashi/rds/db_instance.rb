818cf6b2244c64ffa7d1a00ad2a7b26ff4870170
remain <ponde.ponde.ponde@gmail.com>
2014-06-10 23:47:19 +0900

    Typo :anger:

--- lib/akashi/rds/db_instance.rb
+++ lib/akashi/rds/db_instance.rb
@@ -18,7 +18,7 @@ module Akashi
             master_user_password:       password,
             multi_az:                   !!Akashi.manifest.rds.multi_az,
             availability_zone:          Akashi.manifest.rds.availability_zone,
-            db_subnet_group_nmae:       Akashi.name,
+            db_subnet_group_name:       Akashi.name,
             engine_version:             Akashi.manifest.rds.engine_version,
             auto_minor_version_upgrade: true,
             publicly_accessible:        false,
