77369aaf8397b8ee0521556dfb8593e95a47caf9
remain <ponde.ponde.ponde@gmail.com>
2014-06-10 20:07:55 +0900

    Show result of Akashi::Rds::SubnetGroup.create

--- lib/akashi/rds/subnet_group.rb
+++ lib/akashi/rds/subnet_group.rb
@@ -3,11 +3,14 @@ module Akashi
     class SubnetGroup
       class << self
         def create(subnets:)
-          Akashi::Aws.rds.client.create_db_subnet_group(
+          response = Akashi::Aws.rds.client.create_db_subnet_group(
             db_subnet_group_name:        Akashi.name,
             db_subnet_group_description: Akashi.name,
             subnet_ids:                  subnets.map(&:id)
           )
+          id = response[:db_subnet_group_name]
+
+          puts "Created a SubnetGroup (#{id})."
         end
       end
     end
